ebc98c8cb6fdce84654fd42c233eeba453d51ab7
Ba6taTi <krasizh07@gmail.com>
2024-12-06 14:58:17 +0200

    Statistic file names

--- src/data/handler/DataHandler.java
+++ src/data/handler/DataHandler.java
@@ -3,6 +3,8 @@ package data.handler;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.Calendar;
 import java.util.HashMap;
 
 import coffee.machine.Coffee;
@@ -16,44 +18,44 @@ import static com.sun.management.HotSpotDiagnosticMXBean.ThreadDumpFormat.JSON;
 
 public class DataHandler {
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static final Path statisticsPath = Paths.get("data/statistics/");
     private static final Path consumablesPath = Paths.get("data/consumables.json");
 
     public static void saveStatistics() {
+        Path statisticsPath = Paths.get("data/statistics/" + getDateMonthYear() + ".json");
         HashMap<String, Integer> statistics = Statistics.getDailyStatistic();
         try {
-            objectMapper.writeValue(new File(statisticsPath.toString() + "example.json"), statistics);
+            objectMapper.writeValue(new File(statisticsPath.toString()), statistics);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        // TODO: .json file names
     }
 
     public static HashMap<String,Object> loadConsumables() {
         try {
-            HashMap<String, Object> map = objectMapper.readValue(
-                    new File(consumablesPath.toString()),
-                    HashMap.class
-            );
-            return map;
+            HashMap<String, Object> consumablesHashMap = objectMapper.readValue(new File(consumablesPath.toString()), HashMap.class);
+            return consumablesHashMap;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-
-
-    private static String loadFile(String path) {
-        try {
-            File file = new File(path);
-            FileReader fr = new FileReader(file);
-            BufferedReader readFile = new BufferedReader(fr);
-
-            return readFile.toString();
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
+    private static String getDateMonthYear() {
+        LocalDateTime now = LocalDateTime.now();
+        return now.getMonthValue() + "." + now.getDayOfMonth() + "." + now.getYear();
     }
+
+    // private static String loadFile(String path) {
+    //     try {
+    //         File file = new File(path);
+    //         FileReader fr = new FileReader(file);
+    //         BufferedReader readFile = new BufferedReader(fr);
+
+    //         return readFile.toString();
+
+    //     } catch (FileNotFoundException e) {
+    //         throw new RuntimeException(e);
+    //     }
+
+    // }
+
 }
