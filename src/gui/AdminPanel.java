70ae042c6687a799cd6150f1f0e7adb3058f2245
Ba6taTi <krasizh07@gmail.com>
2024-12-07 17:47:47 +0200

    Admin Panel Base

--- src/gui/AdminPanel.java
+++ src/gui/AdminPanel.java
@@ -1,12 +1,14 @@
 package gui;
 
 import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.JFrame;
 
 public class AdminPanel {
     // TODO
     // * ChatGPT предложи да има един главен панел и два под  
     // * панела (панел за поръчки и администраторскя панел), които да се обедният с CardLayout
+
+    // * За статистките може да използваме JFreeChart или JMathPlot
 
     // CardLayout cardLayout = new CardLayout();
     // JPanel cardPanel = new JPanel(cardLayout);
@@ -23,4 +25,26 @@ public class AdminPanel {
 
     // cardPanel.add(page1, "Page 1");
     // cardPanel.add(page2, "Page 2");
+
+    public static void main(String[] args) { // * Само за тестване
+        JFrame frame = new JFrame("Admin Panel");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 1000);
+        frame.setVisible(true);
+
+        // JButton coffeeManagementButton = new JButton("Coffee Management");
+
+        JButton addCoffeeButton = new JButton("Add Coffee"); // * Евентуален панел за добавяне на кафе или отделен прозорец
+        JButton removeCoffeeButton = new JButton("Remove Coffee");
+        JButton editCoffeeButton = new JButton("Edit Coffee");
+
+        // * Позициите не знам как да ги изчисля
+
+        // Колко и какви консумативи има + зареждане
+
+        frame.add(addCoffeeButton);
+        frame.add(removeCoffeeButton);
+        frame.add(editCoffeeButton);
+    }
 }
