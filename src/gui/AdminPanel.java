e6369bfa6cab956af22dbb359c8334122656c897
Ba6taTi <krasizh07@gmail.com>
2024-12-07 17:36:50 +0200

    AdminPanel Idea

--- src/gui/AdminPanel.java
+++ src/gui/AdminPanel.java
@@ -1,5 +1,26 @@
 package gui;
 
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 public class AdminPanel {
     // TODO
+    // * ChatGPT предложи да има един главен панел и два под  
+    // * панела (панел за поръчки и администраторскя панел), които да се обедният с CardLayout
+
+    // CardLayout cardLayout = new CardLayout();
+    // JPanel cardPanel = new JPanel(cardLayout);
+
+    // JPanel page1 = new JPanel();
+    // page1.add(new JLabel("Page 1"));
+    // JButton toPage2 = new JButton("Go to Page 2");
+    // page1.add(toPage2);
+
+    // JPanel page2 = new JPanel();
+    // page2.add(new JLabel("Page 2"));
+    // JButton toPage1 = new JButton("Go to Page 1");
+    // page2.add(toPage1);
+
+    // cardPanel.add(page1, "Page 1");
+    // cardPanel.add(page2, "Page 2");
 }
